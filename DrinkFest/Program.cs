bfc32bc377449c4fd2588ffa196009630102b9a2
Wale <onafesowale@gmail.com>
2022-09-23 17:31:27 +0100

    Add new file

--- DrinkFest/Program.cs
+++ DrinkFest/Program.cs
@@ -70,7 +70,7 @@ namespace DrinkFest
             //CreateDbIfNotExists(app);
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
